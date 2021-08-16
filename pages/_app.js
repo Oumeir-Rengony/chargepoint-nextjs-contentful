df90f685af3a7c77a1e6f916eb0bd3bfd8c244b5
Mohammad Oumeir Rengony <oumeir.rengony@hangarww.com>
2021-08-16 12:02:29 +0400

    fix styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import { createGlobalStyle } from 'styled-components'
-import TypoStyles from '../styles/typography';
+import { createGlobalStyle } from "styled-components";
+import TypoStyles from "../styles/typography";
 
 const GlobalStyle = createGlobalStyle`
   ${TypoStyles}
@@ -7,12 +7,35 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Gotham-Narrow, Arial, sans-serif';
   }
+
+  body{
+    font-family: 'Gotham-Narrow', Arial, sans-serif;
+  }
+
+  li{
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button{
+    cursor: pointer;
+    border: none;
+    text-decoration: none;
+  }
+
 `;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
